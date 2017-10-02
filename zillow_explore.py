963a79e13fcec9bc41d3b4ecd2b56567a7f9629f
Mark Wu <mang.wu@gmail.com>
2017-10-01 23:51:11 -0400

    year

--- zillow_explore.py
+++ zillow_explore.py
@@ -68,7 +68,7 @@ def calcError(dollar_string, subm_cond):
         if dollar/1000000 >= float(cond[0]) and dollar/1000000 <= float(cond[1]):
             return cond[2]
     return "0.0115"
-    
+ 
 def submit_quant():
     subm_cond = submission_quant.splitlines()
     subm_cond = [ line.strip().split(sep=",") for line in subm_cond if len(line.strip()) != 0]
@@ -92,5 +92,66 @@ def submit_quant():
             line = "{parcel},{mean},{mean},{mean},{mean},{mean},{mean}\n".format(parcel=parcel, mean=logerror)
             fdw.write(line)
 
+def explore_year():
+    rscript = """
+a <- subset(dataset_year, as.integer(dataset_year$year_built) >= {} & as.integer(dataset_year$year_built) < {})
+mean(a$logerror)
+"""
+    other = """
+a <- subset(dataset_year, as.integer(dataset_year$year_built) >= {} | as.integer(dataset_year$year_built) < {})
+mean(a$logerror)
+"""
+    rscripts = [
+        rscript.format("1920", "1930"),
+        rscript.format("1930", "1940"),
+        rscript.format("1940", "1950"),
+        rscript.format("1950", "1960"),
+        rscript.format("1960", "1970"),
+        rscript.format("1970", "1980"),
+        rscript.format("1980", "1990"),
+        rscript.format("1990", "2000"),
+        rscript.format("2000", "2010"),
+        other.format("2010", "1920"),
+    ]
+    print("\n".join(rscripts))
+
+ 
+def year_error(year_s):
+    try:
+        year = int(year_s)
+    except:
+        return "0.0115"
+    if year >= 1920 and year < 1930: return "0.0047"
+    if year >= 1930 and year < 1940: return "0.0055"
+    if year >= 1940 and year < 1950: return "0.0067"
+    if year >= 1950 and year < 1960: return "0.0094"
+    if year >= 1960 and year < 1970: return "0.0166"
+    if year >= 1970 and year < 1980: return "0.0102"
+    if year >= 1980 and year < 1990: return "0.0129"
+    if year >= 1990 and year < 2000: return "0.0136"
+    if year >= 2000 and year < 2010: return "0.0184"
+    if year >= 2010 or year < 1920: return "0.0063"
+    return "0.0115"
+
+def submit_year():
+    properties_file = "/Users/apple/Documents/zillow/data/properties_2016.csv"
+    my_submission = "/Users/apple/Documents/zillow/zillow_submission/my_submission.csv"
+    columns = zillow_columns()
+    with open(properties_file) as fdr, open(my_submission, "w") as fdw:
+        header = fdr.readline()
+        fdw.write("ParcelId,201610,201611,201612,201710,201711,201712\n")
+        processed = 0
+        while True:
+            line = fdr.readline()
+            processed = processed + 1
+            if processed % 100000 == 0: print(processed)
+            column_values = line.split(",")
+            parcel = column_values[0]
+            year = column_values[columns["year_built"]]
+            year = year.split(sep=".")[0]
+            logerror = year_error(year)
+            line = "{parcel},{mean},{mean},{mean},{mean},{mean},{mean}\n".format(parcel=parcel, mean=logerror)
+            fdw.write(line)
+
 if __name__ == "__main__":
-    submit_quant()
+    submit_year()
