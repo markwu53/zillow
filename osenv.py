a463f08bb0939db10d0eb74f4d72fcc1fa9eab44
Mark Wu <mang.wu@gmail.com>
2017-10-10 20:08:41 -0400

    my

--- osenv.py
+++ osenv.py
@@ -1,3 +1,3 @@
 path = "/Programs/kaggle/zillow/"
-path = "/Users/apple/Documents/Programs/zillow/"
 path = "/Users/T162880/Documents/Programs/zillow/"
+path = "/Users/apple/Documents/Programs/zillow/"
