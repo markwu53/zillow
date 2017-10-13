26b3ea50be68bf6729c22c77e788c949b691354f
Mark Wu <mang.wu@gmail.com>
2017-10-12 20:49:02 -0400

    f1

---
+++ trimmed2.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+path = "/Programs/kaggle/zillow/"
+path = "/Users/T162880/Documents/Programs/zillow/"
+path = "/Users/apple/Documents/Programs/zillow/"
+properties_2016 = "properties_2016.csv"
+properties_2017 = "properties_2017.csv"
+train_2016 = "train_2016_v2.csv"
+train_2017 = "train_2017.csv"
+sample_submission = "sample_submission.csv"
+my_submission = "my_submission.csv"
+zillow_columns = """
+parcelid,airconditioningtypeid,architecturalstyletypeid,basementsqft,bathroomcnt,bedroomcnt,buildingclasstypeid,buildingqualitytypeid,calculatedbathnbr,decktypeid,finishedfloor1squarefeet,calculatedfinishedsquarefeet,finishedsquarefeet12,finishedsquarefeet13,finishedsquarefeet15,finishedsquarefeet50,finishedsquarefeet6,fips,fireplacecnt,fullbathcnt,garagecarcnt,garagetotalsqft,hashottuborspa,heatingorsystemtypeid,latitude,longitude,lotsizesquarefeet,poolcnt,poolsizesum,pooltypeid10,pooltypeid2,pooltypeid7,propertycountylandusecode,propertylandusetypeid,propertyzoningdesc,rawcensustractandblock,regionidcity,regionidcounty,regionidneighborhood,regionidzip,roomcnt,storytypeid,threequarterbathnbr,typeconstructiontypeid,unitcnt,yardbuildingsqft17,yardbuildingsqft26,yearbuilt,numberofstories,fireplaceflag,structuretaxvaluedollarcnt,taxvaluedollarcnt,assessmentyear,landtaxvaluedollarcnt,taxamount,taxdelinquencyflag,taxdelinquencyyear,censustractandblock
+"""
+columns = zillow_columns.strip().split(sep=",")
+zcolumns = { value: index for (index, value) in enumerate(columns) }
+
+def load_train():
+    with open(path+train_2016) as fd:
+        lines = [ line.strip().split(",") for line in fd.readlines()[1:]]
+    train_error = { values[0]: float(values[1]) for values in lines }
+    train_set = train_error.keys()
+    train_data = {}
+    with open(path+properties_2016) as fd:
+        header = fd.readline()
+        while True:
+            line = fd.readline()
+            if not line: break
+            values = line.strip().split(",")
+            parcelid = values[0]
+            if parcelid not in train_set: continue
+            train_data[parcelid] = values
+    return train_error, train_set, train_data
+
+train_error, train_set, train_data = load_train()
+trimmed_set = set([ parcelid for parcelid in train_set if abs(train_error[parcelid]-.1) < .5 ])
+trimmed_set_mean = np.mean([ train_error[parcelid] for parcelid in trimmed_set ])
+
+def f_0(values):
+    return trimmed_set_mean
+
+delta_1 = { parcelid: train_error - f_0(train_data[parcelid]) for parcelid in trimmed_set }
+
+def f_1(values):
+    """
+    f_1 is an approximation of delta_1
+    """
+    pass
