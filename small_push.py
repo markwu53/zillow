aa03ef81b08ed4f45f7c58568df82e662be16296
markwu53 <markwu53@gmail.com>
2017-10-16 15:41:27 -0400

    linux

--- small_push.py
+++ small_push.py
@@ -5,6 +5,7 @@ import time
 path = "/Programs/kaggle/zillow/"
 path = "/Users/apple/Documents/Programs/zillow/"
 path = "/Users/T162880/Documents/Programs/zillow/"
+path = "/home/T162880/Programs/zillow/"
 properties_2016 = "properties_2016.csv"
 properties_2017 = "properties_2017.csv"
 train_2016 = "train_2016_v2.csv"
